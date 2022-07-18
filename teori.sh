3d82b97996c770c2dcc9d11223b16492a190bd72
deden2904 <39203619+deden2904@users.noreply.github.com>
2022-07-18 17:52:10 +0700

    Update teori.sh

--- teori.sh
+++ teori.sh
@@ -20,21 +20,15 @@ apt update && apt upgrade -y
 apt install build-essential git curl gcc make jq -y
 
 # Install Go
-ver="1.18.2"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
+wget -c https://go.dev/dl/go1.18.3.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz && rm -rf go1.18.3.linux-amd64.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
 go version
 
 # Setup the chain
-git clone https://github.com/TERITORI/teritori-chain  
-cd teritori-chain 
-git checkout teritori-testnet-v2 
-make install
+git clone https://github.com/TERITORI/teritori-chain && cd teritori-chain && git checkout teritori-testnet-v2 && make install
 teritorid version
 
 # Init the chain
