546cb69ffe3a88890ba2e7d04490e7fdfa098909
deden2904 <39203619+deden2904@users.noreply.github.com>
2022-07-18 16:31:56 +0700

    Create teori.sh

---
+++ teori.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+echo "=================================================="
+echo -e "\033[0;35m"
+echo " :::    ::: ::::::::::: ::::    :::  ::::::::  :::::::::  :::::::::: ::::::::  ";
+echo " :+:   :+:      :+:     :+:+:   :+: :+:    :+: :+:    :+: :+:       :+:    :+: ";
+echo " +:+  +:+       +:+     :+:+:+  +:+ +:+    +:+ +:+    +:+ +:+       +:+        ";
+echo " +#++:++        +#+     +#+ +:+ +#+ +#+    +:+ +#+    +:+ +#++:++#  +#++:++#++ ";
+echo " +#+  +#+       +#+     +#+  +#+#+# +#+    +#+ +#+    +#+ +#+              +#+ ";
+echo " #+#   #+#  #+# #+#     #+#   #+#+# #+#    #+# #+#    #+# #+#       #+#    #+# ";
+echo " ###    ###  #####      ###    ####  ########  #########  ########## ########  ";
+echo -e "\e[0m"
+echo "=================================================="
+
+sleep 2
+
+# update
+apt update && apt upgrade -y 
+
+# Install few packages
+apt install build-essential git curl gcc make jq -y
+
+# Install Go
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+# Setup the chain
+git clone https://github.com/TERITORI/teritori-chain  
+cd teritori-chain 
+git checkout teritori-testnet-v2 
+make install
+teritorid version
+
+# Init the chain
+teritorid init <YOUR_MONIKER> --chain-id teritori-testnet-v2
+
+# Add peers
+sed -i.bak 's/persistent_peers =.*/persistent_peers = "0b42fd287d3bb0a20230e30d54b4b8facc412c53@176.9.149.15:26656,2371b28f366a61637ac76c2577264f79f0965447@176.9.19.162:26656,2f394edda96be07bf92b0b503d8be13d1b9cc39f@5.9.40.222:26656"/' $HOME/.teritorid/config/config.toml
+
+# Download the genesis file
+wget -O ~/.teritorid/config/genesis.json https://raw.githubusercontent.com/TERITORI/teritori-chain/main/testnet/teritori-testnet-v2/genesis.json
+
+# Launch the node
+tee <<EOF >/dev/null /etc/systemd/system/teritorid.service
+[Unit]
+Description=Teritori Cosmos daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=/home/$USER/go/bin/teritorid start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable teritorid
+systemctl daemon-reload
+systemctl restart teritorid
+
+echo '=============== SETUP FINISHED ==================='
+echo -e 'To check logs: \e[1m\e[32mjournalctl -u teritorid.service -f -n 100\e[0m'
